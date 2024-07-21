adf4be5a8ebeb254397fbbc7144f571c8ad1ee7f
Quad3 <bbulat1990@gmail.com>
2024-07-22 02:40:41 +0900

    Change gens

--- src/gen_model.py
+++ src/gen_model.py
@@ -3,19 +3,21 @@ import sys
 import json
 
 
-ROUTER_NAMES = "router_names.txt"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+ROUTER_NAMES = f"{dir_path}/router_names.txt"
+
 
 def gen_model(filename: str) -> None:
     with open(filename) as f:
         data = json.load(f)
 
         try:
-            os.makedirs(f"rest/models/{data['kind']}", exist_ok=True)
+            os.makedirs(f"{dir_path}/rest/models/{data['kind']}", exist_ok=True)
         except OSError as error:
             print(error)
 
-        print(f"mkdir -> rest/models/{data['kind']}")
-        os.system(f"datamodel-codegen --input {filename} --output rest/models/{data['kind']}/model.py")
+        print(f"mkdir -> {dir_path}/rest/models/{data['kind']}")
+        os.system(f"datamodel-codegen --input {filename} --output {dir_path}/rest/models/{data['kind']}/model.py")
         print(f"run -> datamodel-codegen --input {filename} --output model.py")
 
         with open(ROUTER_NAMES, "a") as rf:
--- src/gen_router.py
+++ src/gen_router.py
@@ -1,24 +1,31 @@
 import os
 import sys
+import json
 from jinja2 import Environment, FileSystemLoader
 
 
-def create_router(kind: str):
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    env = Environment(loader=FileSystemLoader('templates'))
+
+def create_router(filename: str) -> None:
+    with open(filename) as f:
+        data = json.load(f)
+        kind = data["kind"]
+
+    env = Environment(loader=FileSystemLoader(f"{dir_path}/templates"))
     template = env.get_template('router_template.py')
     output_from_parsed_template = template.render(kind=kind)
 
     try:
-        os.makedirs(f"rest/routes/{kind}", exist_ok=True)
+        os.makedirs(f"{dir_path}/rest/routes/{kind}", exist_ok=True)
     except OSError as error:
         print(error)
 
-    with open(f"rest/routes/{kind}/router.py", "w") as f:
+    with open(f"{dir_path}/rest/routes/{kind}/router.py", "w") as f:
         f.write(output_from_parsed_template)
-        print(f"Created -> rest/routes/{kind}/router.py")
+        print(f"Created -> {dir_path}/rest/routes/{kind}/router.py")
 
 
 if __name__ == "__main__":
-    kind_name: str = sys.argv[1]
-    create_router(kind_name)
+    filename: str = sys.argv[1]
+    create_router(filename)
